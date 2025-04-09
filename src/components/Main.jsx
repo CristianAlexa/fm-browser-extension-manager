b6ed47b4f2b99c7cf9406112de3f7d58437f08cb
CristianAlexa <cristian.alexa@gmail.com>
2025-04-09 21:58:55 +0100

    style update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import Button from "./ui/Button";
 const Main = () => {
   return (
     <main className="max-w-[1200px] mx-auto">
-      <div className="flex justify-between items-center ">
+      <div className="flex flex-col md:flex-row gap-8 justify-between items-center mb-8 ">
         <h1 className="text-3xl font-semibold dark:text-white">
           Extension List
         </h1>
